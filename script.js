252efe5931b5be17aab68b1dc7146f7cf4a25e0a
gaborio1 <welcome2thepleasuredome@googlemail.com>
2021-04-05 23:46:47 +0200

    add fade in/out showcase, sticky navbar - MacBook

---
+++ script.js
@@ -0,0 +1,32 @@
+// STICKY NAVBAR W3SCHOOLS
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("header");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
+// FADE IN/OUT GALLERY
+let current = 0,
+    slides = document.getElementsByTagName("img");
+
+setInterval(function() {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.opacity = 0;
+  }
+  current = (current != slides.length - 1) ? current + 1 : 0;
+  slides[current].style.opacity = 1;
+}, 8000);
